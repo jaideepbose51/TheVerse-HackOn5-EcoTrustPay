88643357d916aea7235091b18ea1f7be207c5170
jaideepbose51 <jaideepbose51@gmail.com>
2025-06-15 00:21:08 +0530

    compleated server.js

---
+++ backend/config/mongodb.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        mongoose.connection.on('connected', () => {
+            console.log("✅ MongoDB connected successfully.");
+        });
+
+        mongoose.connection.on('error', (err) => {
+            console.error("❌ MongoDB connection error:", err);
+        });
+
+    } catch (error) {
+        console.error("❌ Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
