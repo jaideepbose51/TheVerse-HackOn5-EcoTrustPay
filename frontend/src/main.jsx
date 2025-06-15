c3618441aece2e57423905ef9bf86894070ade7f
meayushsriv <me.facteverse@gmail.com>
2025-06-15 08:31:13 +0530

    created context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <ShopContextProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </ShopContextProvider>
+  </BrowserRouter>
 );
